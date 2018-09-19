5ad6f33c3d7105e6715f0f8dab8a27583c44b306
Lautaro <navarro_lautaro@hotmail.com>
2018-09-19 15:01:02 -0300

    Changed views, index, and requirements

--- mercury_site/mercury_site/views.py
+++ mercury_site/mercury_site/views.py
@@ -2,7 +2,8 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
+from social_django.models import UserSocialAuth
+from eventbrite import Eventbrite
 
 @method_decorator(login_required, name='dispatch')
 class Home(TemplateView, LoginRequiredMixin):
@@ -11,3 +12,25 @@ class Home(TemplateView, LoginRequiredMixin):
     has created """
 
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        context['user'] = self.request.user
+        eventbrite = Eventbrite(get_auth_token(self.request.user))
+        context['events'] = eventbrite.get('/users/me/events/')['events']
+        return context
+
+
+def get_auth_token(user):
+    """
+    This method will receive an user and
+    return its repesctive social_auth token
+    """
+
+    try:
+        token = user.social_auth.get(
+            provider='eventbrite'
+        ).access_token
+    except UserSocialAuth.DoesNotExist:
+        print ('UserSocialAuth does not exists!')
+    return token
