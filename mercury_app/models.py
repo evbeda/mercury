e4285b0b1a2e644e033b884327e87ea5aa8f4209
Milena <milena@eventbrite.com>
2018-09-25 12:10:01 -0300

    Add model Order
    Organization and Event models

--- mercury_app/models.py
+++ mercury_app/models.py
@@ -1,3 +1,45 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    org_id = models.IntegerField()
+    name = models.CharField(max_length=128)
+
+
+class Event(models.Model):
+    organization_id = models.ForeignKey(
+        'Organization',
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1024)
+    event_id = models.IntegerField()
+    url = models.CharField(max_length=128)
+    start_date_tz = models.CharField(max_length=64)
+    start_date_utc = models.DateTimeField()
+    end_date_tz = models.CharField(max_length=64)
+    end_date_utc = models.DateTimeField()
+    created = models.DateTimeField()
+    changed = models.DateTimeField()
+    status = models.CharField(max_length=16)
+
+
+class Order(models.Model):
+    event = models.ForeignKey(
+        'Event',
+        on_delete=models.CASCADE
+    )
+    id_order = models.IntegerField()
+    changed = models.DateTimeField()
+    created = models.DateTimeField()
+    name = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    email = models.EmailField(max_length=300)
+    merchandise_name = models.CharField(max_length=200)
+    currency = models.CharField(max_length=200)
+    value = models.IntegerField()
