303bf3ccfc7a6b3fb8f796dd652491b624939485
Milena <milena@eventbrite.com>
2018-09-25 12:10:01 -0300

    Add model Order

--- mercury_app/models.py
+++ mercury_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    event = models.ForeignKey(
+        'Event',
+        on_delete=models.CASCADE
+    )
+    id_order = models.IntegerField()
+    changed = models.DateTimeField()
+    created = models.DateTimeField()
+    name = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    email = models.EmailField(max_length=300)
+    merchandise_name = models.CharField(max_length=200)
+    currency = models.CharField(max_length=200)
+    value = models.IntegerField()
