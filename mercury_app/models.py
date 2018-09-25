9c1104776d832f836b646bae7fcf84aa2ed71f38
Milena <milena@eventbrite.com>
2018-09-25 12:10:01 -0300

    Add model Order
    Organization and Event models

--- mercury_app/models.py
+++ mercury_app/models.py
@@ -20,9 +20,8 @@ class Event(models.Model):
     description = models.CharField(max_length=1024)
     event_id = models.IntegerField()
     url = models.CharField(max_length=128)
-    start_date_tz = models.CharField(max_length=64)
+    date_tz = models.CharField(max_length=64)
     start_date_utc = models.DateTimeField()
-    end_date_tz = models.CharField(max_length=64)
     end_date_utc = models.DateTimeField()
     created = models.DateTimeField()
     changed = models.DateTimeField()
@@ -30,16 +29,24 @@ class Event(models.Model):
 
 
 class Order(models.Model):
-    event = models.ForeignKey(
+    event_id = models.ForeignKey(
         'Event',
         on_delete=models.CASCADE
     )
     id_order = models.IntegerField()
     changed = models.DateTimeField()
     created = models.DateTimeField()
-    name = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
-    email = models.EmailField(max_length=300)
-    merchandise_name = models.CharField(max_length=200)
-    currency = models.CharField(max_length=200)
+    name = models.CharField(max_length=256)
+    status = models.CharField(max_length=32)
+    email = models.EmailField(max_length=256)
+
+
+class Merchandise(models.Model):
+    order_id = models.ForeignKey(
+        'Order',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=256)
+    item_type = models.CharField(max_length=128)
+    currency = models.CharField(max_length=16)
     value = models.IntegerField()
