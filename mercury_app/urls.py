d2eac32e3fa106d8a9855c3140f7cd0819d3b00c
Lautaro <navarro_lautaro@hotmail.com>
2018-09-26 12:10:31 -0300

    Added view to select events

--- mercury_app/urls.py
+++ mercury_app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from mercury_app.views import Home
+from mercury_app.views import Home, SelectEvents
 
 urlpatterns = [
-    url(r'^$', Home.as_view(template_name='index.html'), name='index'),
+    url(r'^select_events/', SelectEvents.as_view(template_name='select_events.html'), name='select_events'),
+    url(r'^(?P<message>[a-zA-Z]+)?', Home.as_view(template_name='index.html'), name='index'),
 ]
