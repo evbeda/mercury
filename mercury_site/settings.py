c8fba15e4e62d9dd30af4d0967ee99751c764d53
Lautaro <navarro_lautaro@hotmail.com>
2018-09-27 12:25:03 -0300

    Added test of Home,the the path of static_file_storage and the files for coverage

--- mercury_site/settings.py
+++ mercury_site/settings.py
@@ -162,10 +162,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 DB_FROM_ENV = dj_database_url.config(conn_max_age=500)
 
