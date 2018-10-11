9e771d698717debc8e2f3589d1edc0686cebe021
Milena <milena@eventbrite.com>
2018-10-11 09:57:56 -0300

    add fields and fontawesome

--- mercury_app/tables.py
+++ mercury_app/tables.py
@@ -1,6 +1,9 @@
 import django_tables2 as tables
 from mercury_app.models import Order
 from django.utils.html import format_html
+from fontawesome.fields import IconField
+from django.utils.safestring import mark_safe
+
 
 
 class OrderTable(tables.Table):
@@ -9,11 +12,12 @@ class OrderTable(tables.Table):
     actions = tables.Column(
         verbose_name='Actions',
         orderable=False,
-        empty_values=()
+        empty_values=(),
+        attrs={"th": {"style": "text-align: justify"}},
     )
 
     def render_actions(self, value, record):
-        return format_html('<a href="/view_order/{}/" class="btn btn-outline-success">View</a>'.format(record.id))
+        return format_html('<ul class="fa-ul fa-2x row"><a href="/view_order/{}/"><li><i class="fa fa-eye"></i></li></a> &nbsp &nbsp &nbsp <a href=""><li> <i class="fa fa-trash"></i> </li></ul></a>'.format(record.id))
 
     class Meta:
         model = Order
