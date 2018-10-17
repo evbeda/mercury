28f8feeeeefabf65aa1d81471dfdda592dee25e8
MilenaFigueres <figueresmilena@gmail.com>
2018-10-17 15:50:55 -0300

    Add button delete, test, pretty date and delete navbar error.

--- mercury_app/urls.py
+++ mercury_app/urls.py
@@ -7,12 +7,14 @@ from mercury_app.views import (
     Summary,
     FilteredOrderListView,
     ScanQRView,
+    DeleteEvents,
 )
 
 
 urlpatterns = [
     url(r'event/(?P<event_id>\d+)/summary/$', Summary.as_view(template_name='summary.html'), name='summary'),
     url(r'event/(?P<event_id>\d+)/scanqr/$', ScanQRView.as_view(template_name='scanqr.html'), name='scanqr'),
+    url(r'event/(?P<event_id>\d+)/delete/$', DeleteEvents.as_view(template_name='delete.html'), name='delete'),
 	url(r'^webhook-point/',accept_webhook,name='accept_webhook'),
 	url(r'view_order/(?P<order_id>\d+)/$', ListItemMerchandising.as_view(template_name='list_item_mercha.html'), name='item_mercha'),
     url(r'event/(?P<event_id>\d+)/orders/$', FilteredOrderListView.as_view(), name='orders'),
